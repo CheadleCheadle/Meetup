30d0c63d053ec3113990dbc086a5b70a4f2e485d
Grant <gcheadle123@gmail.com>
2023-02-15 14:43:38 -0800

    added error handlers

--- authenticate-me/backend/app.js
+++ authenticate-me/backend/app.js
@@ -5,7 +5,6 @@ const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
-
 const { environment } = require('./config');
 const isProduction = environment === 'production';
 
@@ -18,13 +17,11 @@ app.use(express.json());
 if (!isProduction) {
     app.use(cors());
 }
-
 app.use(
     helmet.crossOriginResourcePolicy({
         policy: "cross-origin"
     })
 );
-
 app.use(
     csurf({
       cookie: {
@@ -34,9 +31,44 @@ app.use(
       }
     })
 )
-
 const routes = require('./routes');
-
 app.use(routes);
+//Resource not found Error Handler
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = ["The requested resource couldn't be found"];
+    err.status = 404;
+    next(err);
+});
+
+//Sequelize Error-Handler
+const { ValidationError } = require('sequelize');
+const { escapeRegExp } = require('lodash');
+
+app.use((err, _req, _res, next) => {
+    if (err instanceof ValidationError) {
+        err.errors = err.errors.map((e) => e.message);
+        err.title = 'Validaton error';
+    }
+    next(err);
+});
+
+//Error Formatter Error-Handler
+
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+        title: err.title || 'Server Error',
+        message: err.message,
+        errors: err.errors,
+        stack: isProduction ? null : err.stack
+    });
+});
+
+
+
+
 
 module.exports = app;
