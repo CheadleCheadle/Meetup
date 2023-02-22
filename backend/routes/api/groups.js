921ba0abd029a9c4a48be052eaf48752e388faec
Grant <gcheadle123@gmail.com>
2023-02-21 19:35:48 -0800

    debug

--- backend/routes/api/groups.js
+++ backend/routes/api/groups.js
@@ -26,20 +26,20 @@ const { requireAuth } = require('../../utils/auth');
 router.get('/',  async(req, res) => {
     const groups = await Group.findAll();
 
-    for (let i = 0; i < groups.length; i++) {
-        const members = await Membership.findAll({
-            where: {groupId: groups[i].id}
-        })
-        const image = await GroupImage.findOne({
-            where: {groupId : groups[i].id}
-        })
-        groups[i].dataValues.numMembers = members.length;
+//     for (let i = 0; i < groups.length; i++) {
+//         const members = await Membership.findAll({
+//             where: {groupId: groups[i].id}
+//         })
+//         const image = await GroupImage.findOne({
+//             where: {groupId : groups[i].id}
+//         })
+//         groups[i].dataValues.numMembers = members.length;
 
-        if (image) {
-        groups[i].dataValues.previewImage = image.url
-        }
+//         if (image) {
+//         groups[i].dataValues.previewImage = image.url
+//         }
 
-}
+// }
     res.status(200).json({Groups:groups});
 });
 
@@ -60,18 +60,18 @@ router.get('/current', requireAuth, async (req, res) => {
         })
          console.log(groups[0].dataValues);
 
-        for (let i = 0; i < groups.length; i++) {
-            const members = await Membership.findAll({
-                where: {
-                    groupId: groups[i].dataValues.id
-                }
-            });
-            const image = await GroupImage.findOne({
-                where: {groupId: groups[i].dataValues.id}
-            })
-            groups[i].dataValues.numMembers = members.length;
-            groups[i].dataValues.previewImage = image.url;
-        }
+        // for (let i = 0; i < groups.length; i++) {
+        //     const members = await Membership.findAll({
+        //         where: {
+        //             groupId: groups[i].dataValues.id
+        //         }
+        //     });
+        //     const image = await GroupImage.findOne({
+        //         where: {groupId: groups[i].dataValues.id}
+        //     })
+        //     groups[i].dataValues.numMembers = members.length;
+        //     groups[i].dataValues.previewImage = image.url;
+        // }
 
         //  console.log(memberships[0].Group.dataValues);
         res.status(200).json({Groups:groups});
@@ -101,34 +101,38 @@ router.get('/:groupId', async (req, res) => {
         res.status(404).json({message: "Group couldn't be found", statusCode: 404});
     }
 });
+/*
+Having trouble creating new membership when creating new group.
+Check error and ask a question on Slack.
 
+*/
 router.post('/', requireAuth, async (req, res) => {
     const {name, about, type, private, city, state } = req.body;
-    const { user } = req;
-    try {
+    // const { user } = req;
+    // try {
     const newGroup = await Group.create({name, about, type, private, city, state, organizerId: user.dataValues.id});
     //create new membership
     // console.log(user.id,  newGroup.dataValues.id);
-    const member = await Membership.create({userId:user.dataValues.id, groupId:newGroup.dataValues.id, status: "Host"});
+    // const member = await Membership.create({userId:user.dataValues.id, groupId:newGroup.dataValues.id, status: "Host"});
 
-     console.log(member);
+    //  console.log(member);
     res.status(201).json(newGroup);
-    } catch (e) {
-        console.log(e);
-        res.status(400).json({
-            message: "Validation Error",
-            statusCode: 400,
-            error: {
-                name: "Name must be 60 characters or less",
-                about: "About must be 50 characters or more",
-                type: "Type must be 'Online' or 'In person'",
-                private: "Private must be a boolean",
-                city: "City is required",
-                state: "State is required",
-                error: e
-            }
-        })
-    }
+    // } catch (e) {
+        // console.log(e);
+        // res.status(400).json({
+        //     message: "Validation Error",
+        //     statusCode: 400,
+        //     error: {
+        //         name: "Name must be 60 characters or less",
+        //         about: "About must be 50 characters or more",
+        //         type: "Type must be 'Online' or 'In person'",
+        //         private: "Private must be a boolean",
+        //         city: "City is required",
+        //         state: "State is required",
+        //         error: e
+        //     }
+        // })
+    // }
 });
 
 //Add an Image to a Group based on the Group's id
