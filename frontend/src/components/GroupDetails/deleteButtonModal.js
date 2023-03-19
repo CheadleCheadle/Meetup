18eed389c9d7f986df6d969433f2e10a27958eee
Grant <gcheadle123@gmail.com>
2023-03-19 13:51:38 -0700

    final before changing fonts etc...

--- frontend/src/components/GroupDetails/deleteButtonModal.js
+++ frontend/src/components/GroupDetails/deleteButtonModal.js
@@ -3,6 +3,7 @@ import { useDispatch } from "react-redux";
 import { useModal } from "../../context/Modal";
 import { deleteGroupAction } from "../../store/groups";
 import { useHistory } from "react-router-dom";
+import "./deleteButton.css";
 export default function DeleteButtonModal({groupId}) {
     const history = useHistory();
     const dispatch = useDispatch();
@@ -14,11 +15,15 @@ export default function DeleteButtonModal({groupId}) {
     }
 
     return (
-        <div>
-        <h3>Confirm Delete</h3>
-        <p> Are you sure you want to remove this group?</p>
-        <button onClick={() => handleDelete(groupId)}>Yes</button>
-        <button onClick={closeModal}>No</button>
+        <div className="delete-group">
+        <span className="header-text">
+        <h1>Confirm Delete</h1>
+        </span>
+        <h3> Are you sure you want to remove this group?</h3>
+        <div className="button-holder">
+        <button id="delete-group-yes" onClick={() => handleDelete(groupId)}>Yes (Delete Group)</button>
+        <button id="delete-group-no" onClick={closeModal}>No (Keep Group)</button>
+        </div>
         </div>
     )
 }
